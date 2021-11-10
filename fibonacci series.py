80297291838e8626cbfe9cd844b3bd5c2334c10a
karthi482 <93927810+karthi482@users.noreply.github.com>
2021-11-10 22:58:55 +0530

    Add files via upload

---
+++ fibonacci series.py
@@ -0,0 +1,30 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[18]:
+
+
+a = 0
+b = 1
+n = 1
+l = int(input("enter the limit :"))
+print(a,"",b,end="")
+while n<=8:
+    val = a + b
+    print("",val,end="")
+    a = b
+    b = val
+    n = n+1
+
+
+# In[7]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
