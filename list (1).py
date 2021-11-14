22d396ce83502e7dd687edddabb326725f196236
karthi482 <93927810+karthi482@users.noreply.github.com>
2021-11-14 20:36:14 +0530

    Add files via upload

---
+++ list (1).py
@@ -0,0 +1,26 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[1]:
+
+
+def last(n): return n[-1]
+
+def sort_list_last(tuples):
+  return sorted(tuples, key=last)
+
+print(sort_list_last([(2, 5), (1, 2), (4, 4), (2, 3), (2, 1)]))
+
+
+# In[2]:
+
+
+my_dict = {chr(i): i for i in range(97, 97 + 26)}
+print(my_dict)
+
+
+# In[ ]:
+
+
+
+
