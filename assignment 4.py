4c1c8426187dc6bc0d1d42953ff419da309b87e5
karthi482 <93927810+karthi482@users.noreply.github.com>
2021-12-05 20:53:34 +0530

    Add files via upload

---
+++ assignment 4.py
@@ -0,0 +1,39 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[3]:
+
+
+# Play with Lambda
+x=lambda a: a+25
+print(x(10))
+
+
+# In[7]:
+
+
+# Find the way with Maps
+num=[1,2,3,4,5,6,7]
+y=list(map(lambda x:x+x+x,num))
+print(list(y))
+
+
+# In[4]:
+
+
+# Find the Squares from the given List
+x=[4,5,2,9]
+(list(map(lambda a:a*a, x)))
+
+
+# In[6]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
